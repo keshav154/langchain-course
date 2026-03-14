4f5478c1eecaf8f67d0b58d489aedf8fdc9f8239
Keshav_saxena <saxena.keshav3@gmail.com>
2026-03-15 00:46:07 +0530

    implement pydantic validation schema

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
+from typing import List
+from pydantic import BaseModel, Field
+class Source(BaseModel):
+    """
+    Schema for the source used by the agent
+    """
+    url: str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """
+    Schema for the response returned by the agent
+    """
+    answer: str = Field(description="The answer to the user's query")
+    sources: List[Source] = Field(description="List of sources used to generate the answer")
+
 # from tavily import TavilyClient
 load_dotenv()
 # tavily = TavilyClient()
@@ -19,10 +34,10 @@ load_dotenv()
 #     return tavily.search(query=query, num_results=5)
 llm = ChatOpenAI()
 tools=[TavilySearch(num_results=1)]
-agent = create_agent(llm, tools=tools)
+agent = create_agent(llm, tools=tools, response_format=AgentResponse)
 def main():
     print("Hello from langchain-course!")
-    response = agent.invoke({"messages": [HumanMessage(content="I want to get the listing of job openings in Linkeding for Gen AI engineer in Noida, India with their job description")]})
+    response = agent.invoke({"messages": [HumanMessage(content="I want to get the latest 2 listing of job openings for Gen AI engineer in Noida, India with their job description")]})
     print(response)
 if __name__ == "__main__":
     main()
--- main1.py
+++ main1.py
@@ -4,13 +4,30 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_ollama import ChatOllama
 from langchain_tavily import TavilySearch
+from typing import List
+from pydantic import BaseModel, Field
+from langchain.agents.structured_output import ToolStrategy
+class Source(BaseModel):
+    """
+    Schema for the source used by the agent
+    """
+    url: str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """
+    Schema for the response returned by the agent
+    """
+    answer: str = Field(description="The answer to the user's query")
+    sources: List[Source] = Field(description="List of sources used to generate the answer")
+
+
 load_dotenv()
 llm = ChatOllama(model="qwen3.5:0.8b")
 tools=[TavilySearch(num_results=1)]
-agent = create_agent(llm, tools=tools)
+agent = create_agent(llm, tools=tools, response_format=ToolStrategy(AgentResponse))
 def main():
     print("Hello from AI agent")
-    response = agent.invoke({"messages": [HumanMessage(content="Give me the two best job openings in Noida Uttar pradesh for Senios devops engineer positions on Linkedin mentioning the URL and description of it")]})
+    response = agent.invoke({"messages": [HumanMessage(content="Give me the two best job listings in Noida Uttar pradesh for Senios devops engineer positions on Linkedin mentioning the URL and description of it")]})
     print(response)
 if __name__ == "__main__":
     main()
